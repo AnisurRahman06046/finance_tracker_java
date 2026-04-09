955b4327cb95871f004218c694922d25c93c84db
AnisurRahman06046 <anisurrahman06046@gmail.com>
2026-04-09 21:39:45 +0600

    feat/Transaction: entity is created

---
+++ src/main/java/com/capstone/financeTracker/entity/Transaction.java
@@ -0,0 +1,33 @@
+package com.capstone.financeTracker.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="transactions")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Transaction {
+    @Id
+    private Long id;
+    private double amount;
+    private String type;
+    private String description;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+}
